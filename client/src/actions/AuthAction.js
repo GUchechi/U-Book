c76d45c1cd8ebbbc76243014456758bece8ce26d
GUchechi <godswilluchendu7@gmail.com>
2023-04-13 16:34:38 +0100

    auth

--- client/src/actions/AuthAction.js
+++ client/src/actions/AuthAction.js
@@ -1,9 +1,9 @@
 import * as AuthApi from "../api/AuthRequest";
 
-export const logIn = (formData) => async (dispatch) => {
+export const signUp = (formData) => async (dispatch) => {
   dispatch({ type: "AUTH_START" });
   try {
-    const { data } = await AuthApi.logIn(formData);
+    const { data } = await AuthApi.signUp(formData);
     dispatch({ type: "AUTH_SUCCESS", data: data });
   } catch (error) {
     console.log(error);
@@ -11,10 +11,10 @@ export const logIn = (formData) => async (dispatch) => {
   }
 };
 
-export const signUp = (formData) => async (dispatch) => {
+export const logIn = (formData) => async (dispatch) => {
   dispatch({ type: "AUTH_START" });
   try {
-    const { data } = await AuthApi.signUp(formData);
+    const { data } = await AuthApi.logIn(formData);
     dispatch({ type: "AUTH_SUCCESS", data: data });
   } catch (error) {
     console.log(error);
