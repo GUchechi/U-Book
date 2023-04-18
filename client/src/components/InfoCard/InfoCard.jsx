aeb086323fea393b320816c2969e17b85e863ac9
GUchechi <godswilluchendu7@gmail.com>
2023-04-19 00:19:44 +0100

    logOut

--- client/src/components/InfoCard/InfoCard.jsx
+++ client/src/components/InfoCard/InfoCard.jsx
@@ -1,49 +1,82 @@
-import { useState } from "react";
+import * as UserApi from "../../api/UserRequest.js";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
 import { UilPen } from "@iconscout/react-unicons";
 import ProfileModal from "../ProfileModal/ProfileModal";
+import { logOut } from "../../actions/AuthAction.js";
 import "./InfoCard.css";
 
 const InfoCard = () => {
   const [modalOpened, setModalOpened] = useState(false);
+  const { user } = useSelector((state) => state.authReducer.authData);
+  const dispatch = useDispatch();
+  const params = useParams();
+  const [profileUser, setProfileUser] = useState({});
+  const profileUserId = params.id;
+
+  useEffect(() => {
+    const fetchProfileUser = async () => {
+      if (profileUserId === user._id) {
+        setProfileUser(user);
+      } else {
+        const profileUser = await UserApi.getUser(profileUserId);
+        setProfileUser(profileUser);
+      }
+    };
+
+    fetchProfileUser();
+  }, [user]);
+
+  // Logout
+  const handleLogOut = () => {
+    dispatch(logOut());
+  };
 
   return (
     <div className="infoCard">
       <div className="infoHead">
-        <h4>Your Info</h4>
-        <div>
-          <UilPen
-            width="2rem"
-            height="1.2rem"
-            onClick={() => setModalOpened(true)}
-          />
-          <ProfileModal
-            modalOpened={modalOpened}
-            setModalOpened={setModalOpened}
-          />
-        </div>
+        <h4>Profile Info</h4>
+        {user._id === profileUserId ? (
+          <div>
+            <UilPen
+              width="2rem"
+              height="1.2rem"
+              onClick={() => setModalOpened(true)}
+            />
+            <ProfileModal
+              modalOpened={modalOpened}
+              setModalOpened={setModalOpened}
+            />
+          </div>
+        ) : (
+          ""
+        )}
       </div>
 
       <div className="info">
         <span>
           <b>Status </b>
         </span>
-        <span>In a RelationShip</span>
+        <span>{profileUser.relationShip}</span>
       </div>
 
       <div className="info">
         <span>
           <b>Lives in </b>
         </span>
-        <span>France</span>
+        <span>{profileUser.livesIn}</span>
       </div>
 
       <div className="info">
         <span>
           <b> Works at </b>
         </span>
-        <span> MicroSoft</span>
+        <span> {profileUser.worksAt}</span>
       </div>
-      <button className="button logout-button">Logout</button>
+      <button className="button logout-button" onClick={handleLogOut}>
+        Logout
+      </button>
     </div>
   );
 };
