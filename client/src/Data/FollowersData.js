e32eb7d34810d263a7483b5588f21506ea55e2f8
GUchechi <godswilluchendu7@gmail.com>
2023-04-21 00:41:55 +0100

    followers data

--- client/src/Data/FollowersData.js
+++ client/src/Data/FollowersData.js
@@ -5,7 +5,10 @@ import img4 from "../img/img4.jpg";
 
 export const Followers = [
   { name: "Andrew Thomas", username: "AndrewThomas", img: img1 },
+
   { name: "Hulk Buster", username: "HulkBuster", img: img2 },
+
   { name: "Thor", username: "ThunderMaster", img: img3 },
+
   { name: "Natasha", username: "Natasha", img: img4 },
 ];
