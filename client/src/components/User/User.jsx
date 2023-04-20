b25df6ad1c7e9dff4b9f77023922459d2aa395cc
GUchechi <godswilluchendu7@gmail.com>
2023-04-20 01:58:19 +0100

    following

--- client/src/components/User/User.jsx
+++ client/src/components/User/User.jsx
@@ -1,7 +1,22 @@
-import React from "react";
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { followUser, unFollowUser } from "../../actions/userAction";
 
 const User = ({ person }) => {
+  const dispatch = useDispatch();
   const serverPublic = process.env.REACT_APP_PUBLIC_FOLDER;
+  const { user } = useSelector((state) => state.authReducer.authData);
+  const [following, setFollowing] = useState(
+    person.following.includes(user._id)
+  );
+
+  const handleFollow = () => {
+    following
+      ? dispatch(unFollowUser(person._id, user))
+      : dispatch(followUser(person._id, user));
+    setFollowing((prev) => !prev);
+  };
+
   return (
     <div className="follower">
       <div>
@@ -19,7 +34,9 @@ const User = ({ person }) => {
           <span>{person.username}</span>
         </div>
       </div>
-      <button className="button fc-button">Follow</button>
+      <button className="button fc-button" onClick={handleFollow}>
+        {following ? "Unfollow" : "Follow"}
+      </button>
     </div>
   );
 };
--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -26,6 +26,33 @@ const authReducer = (
     case "LOG_OUT":
       localStorage.clear();
       return { ...state, authData: null, loading: false, error: false };
+    case "FOLLOW_USER":
+      return {
+        ...state,
+        authData: {
+          ...state.authData,
+          user: {
+            ...state.authData.user,
+            following: [...state.authData.user.following, action.data],
+          },
+        },
+      };
+
+    case "UNFOLLOW_USER":
+      return {
+        ...state,
+        authData: {
+          ...state.authData,
+          user: {
+            ...state.authData.user,
+            following: [
+              ...state.authData.user.following.filter(
+                (personId) => personId !== action.data
+              ),
+            ],
+          },
+        },
+      };
     default:
       return state;
   }
