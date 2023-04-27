59395614db560cc3f73acb7aba61ff06d472f497
GUchechi <godswilluchendu7@gmail.com>
2023-04-27 08:43:48 +0100

    getting chats

--- client/src/Pages/Chat/Chat.jsx
+++ client/src/Pages/Chat/Chat.jsx
@@ -1,11 +1,44 @@
-import './Chat.css'
+import { useState, useEffect } from "react";
+import { useSelector } from "react-redux";
+import LogoSearch from "../../components/LogoSearch/LogoSearch";
+import { userChats } from "../../api/ChatRequest";
+import "./Chat.css";
 
 const Chat = () => {
-  return (
-    <div>
-      Chat
-    </div>
-  )
-}
+  const [chats, setChats] = useState([]);
+  const { user } = useSelector((state) => state.authReducer.authData);
 
-export default Chat
+  // Get the chat in chat section
+  useEffect(() => {
+    const getChats = async () => {
+      try {
+        const { data } = await userChats(user._id);
+        setChats(data);
+        console.log(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getChats();
+  }, [user]);
+
+  return (
+    <div className="Chat">
+      {/* Left Side */}
+      <div className="Left-side-chat">
+        <LogoSearch />
+        <div className="Chat-container">
+          <h2>Chats</h2>
+          <div className="Chat-list">Conversation</div>
+        </div>
+      </div>
+
+      {/* Right Side */}
+      <div className="Right-side-chat">
+        <h2>Chats</h2>
+      </div>
+    </div>
+  );
+};
+
+export default Chat;
