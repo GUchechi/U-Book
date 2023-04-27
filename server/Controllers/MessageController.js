1d5ce649fb74b75330e0c7be354245b11a033c18
GUchechi <godswilluchendu7@gmail.com>
2023-04-27 07:34:41 +0100

    chat request

---
+++ server/Controllers/MessageController.js
@@ -0,0 +1,14 @@
+import MessageModel from "../Models/MessageModel";
+
+export const addMessage = async (req, res) => {
+  const { chatId, senderId, text } = req.body;
+  const message = new MessageModel({ chatId, senderId, text });
+
+  try {
+    const result = await message.save();
+    res.status(200).json(result);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
+
---
+++ server/Routes/MessageRoute.js
@@ -0,0 +1,28 @@
+import express  from "express";
+
+const router = express.Router()
+
+router.post('/', addMessage)
+router.get('/:chatId', getMessages)
+
+export default router
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
