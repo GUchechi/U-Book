b818a9956727477b77b90bb5035d8f950ab92deb
GUchechi <godswilluchendu7@gmail.com>
2023-04-28 06:51:06 +0100

    chatbox

--- client/src/Pages/Chat/Chat.jsx
+++ client/src/Pages/Chat/Chat.jsx
@@ -1,5 +1,10 @@
 import { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import Noti from "../../img/noti.png";
+import Comment from "../../img/comment.png";
+import { UilEstate } from "@iconscout/react-unicons";
+import { UilSetting } from "@iconscout/react-unicons";
 import LogoSearch from "../../components/LogoSearch/LogoSearch";
 import { userChats } from "../../api/ChatRequest";
 import Conversation from "../../components/Conversation/Conversation";
@@ -7,6 +12,7 @@ import "./Chat.css";
 
 const Chat = () => {
   const [chats, setChats] = useState([]);
+  const [currentChat, setCurrentChat] = useState(null);
   const { user } = useSelector((state) => state.authReducer.authData);
 
   // Get the chat in chat section
@@ -32,7 +38,11 @@ const Chat = () => {
           <h2>Chats</h2>
           <div className="Chat-list">
             {chats.map((chat) => (
-              <div>
+              <div
+                onClick={() => {
+                  setCurrentChat(chat);
+                }}
+              >
                 <Conversation data={chat} currentUserId={user._id} />
               </div>
             ))}
@@ -42,7 +52,21 @@ const Chat = () => {
 
       {/* Right Side */}
       <div className="Right-side-chat">
-       
+        <div style={{ width: "20rem", alignSelf: "flex-end" }}>
+          <div className="navIcons">
+            <Link to={"../home"}>
+              <UilEstate size="26.5" />
+            </Link>
+            <UilSetting size="26.5" />
+            <img src={Noti} alt="" />
+            <Link to="../chat">
+              <img src={Comment} alt="" />
+            </Link>
+          </div>
+
+          {/* Chat Body */}
+          <ChatBox chat={currentChat} currentUser={user._id} />
+        </div>
       </div>
     </div>
   );
