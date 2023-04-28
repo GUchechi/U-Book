fc6e45b0c2a1e051e36ced65331dd26921506f91
GUchechi <godswilluchendu7@gmail.com>
2023-04-28 07:15:35 +0100

    message Request

--- client/src/components/ChatBox/ChatBox.jsx
+++ client/src/components/ChatBox/ChatBox.jsx
@@ -1,11 +1,33 @@
-import React from 'react'
+import { useEffect } from "react";
+import { useState } from "react";
+import { getUser } from "../../api/UserRequest";
 
-const ChatBox = () => {
+const ChatBox = ({ chat, currentUser }) => {
+  const [userData, setUserData] = useState(null);
+  const [messages, setMessages] = useState([]);
+  const [newMessage, setNewMessage] = useState("");
+
+  // fetching data for header
+  useEffect(() => {
+    const userId = chat?.members?.find((id) => id !== currentUser);
+    const getUserData = async () => {
+      try {
+        const { data } = await getUser(userId);
+        console.log(data)
+        setUserData(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    if (chat !== null) getUserData();
+  }, [chat, currentUser]);
+
   return (
-    <div>
-      ChatBox
-    </div>
-  )
-}
+    <>
+      <div className="ChatBox-container"></div>
+    </>
+  );
+};
 
-export default ChatBox
+export default ChatBox;
