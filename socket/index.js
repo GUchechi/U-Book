39d3f2b50ce75e653101e6d101cd26c257793413
GUchechi <godswilluchendu7@gmail.com>
2023-07-29 01:25:24 +0100

    socket

--- socket/index.js
+++ socket/index.js
@@ -27,13 +27,6 @@ io.on("connection", (socket) => {
   });
 
   // send message to a specific user
-  socket.on("send-message", (data) => {
-    const { receiverId } = data;
-    const user = activeUsers.find((user) => user.userId === receiverId);
-    console.log("Sending from socket to :", receiverId);
-    console.log("Data: ", data);
-    if (user) {
-      io.to(user.socketId).emit("recieve-message", data);
-    }
+ 
   });
 });
