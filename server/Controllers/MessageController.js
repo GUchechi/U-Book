6837e82e675cb5e06a0483648a7ef5fc72329336
GUchechi <godswilluchendu7@gmail.com>
2023-08-01 01:27:06 +0100

    refactoring

--- server/Controllers/MessageController.js
+++ server/Controllers/MessageController.js
@@ -4,12 +4,7 @@ export const addMessage = async (req, res) => {
   const { chatId, senderId, text } = req.body;
   const message = new MessageModel({ chatId, senderId, text });
 
-  try {
-    const result = await message.save();
-    res.status(200).json(result);
-  } catch (error) {
-    res.status(500).json(error);
-  }
+  
 };
 
 export const getMessages = async (req, res) => {
